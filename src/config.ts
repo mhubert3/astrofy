c4dc53ff7ef040c115ea7e1b3c2bb499ee94d2c1
mhubert3 <146325770+mhubert3@users.noreply.github.com>
2025-06-03 07:11:41 -0400

    Updated a few descriptions on website

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Morgan Huberty | Personal Website & Portfolio';
+export const SITE_DESCRIPTION = 'Currently a sophomore stuedent at Johns Hopkins University studying Computer Engineering. Major-relevant interests include AI, robotics, and computer architecture. General interests include health, history, film, and philosophy';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
