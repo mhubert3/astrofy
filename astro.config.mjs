8b931178e1fd712d891892ac693355340e0eaafd
mhubert3 <146325770+mhubert3@users.noreply.github.com>
2025-08-13 02:12:27 -0400

    Update site configuration in astro.config.mjs to correct the site URL format and reset the base path to the root for improved routing consistency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import tailwind from "@astrojs/tailwind";
 const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  site: 'https://mhubert3.github.io',
-  base: isProd ? '/astrofy' : '/',
+  site: 'https://mhubert3.github.io/',
+  base: '/',
   trailingSlash: 'ignore',
   integrations: [mdx(), sitemap(), tailwind()]
 });
